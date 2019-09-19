e9dd0f6a2b4cdc68c2101aec47dfa27901f691a2
noam sauer-utley <noamsauerutley@gmail.com>
2019-09-19 18:35:27 +0000

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,35 @@
-class Animal
 
+# ### Animal Class
+## - An `animal` should be instantiated with the species (e.g. "Cat", "Dog", "Rat"), a numerical weight and a nickname. Keep in mind that the animal's species and nickname should not be able to change, but its weight can.
+## - `Animal#nickname` should return the nickname of the animal.
+## - `Animal#weight` should return the weight of the animal.
+## - `Animal#species` should return the species of the animal.
+## - `Animal.all` should return an array of all the animal instances.
+## - `Animal#zoo` should return the zoo instance that the instance belongs to.
+## - `Animal.find_by_species` should take in an animal's species as an argument ansearray of all the animals, which are of that species.
+
+class Animal
+    attr_reader :species, :nickname, :zoo
+    attr_accessor :weight
+
+    @@all = []
+
+    def initialize (zoo, species, weight, nickname)
+        @zoo = zoo
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(animal_species)
+        @@all.select do |animal|
+            animal.species == animal_species
+        end
+        
+    end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,20 @@ require 'pry'
 
 
 #Test your code here
+brooklyn_zoo = Zoo.new("Brooklyn Municipal Zoo", "Brooklyn")
+san_diego_zoo = Zoo.new("San Diego Zoo", "San Diego")
+chicago_zoo = Zoo.new("Chicago City Zoo", "Chicago")
+franklin = Animal.new(brooklyn_zoo, "zebra", 500, "frankie")
+howard = Animal.new(brooklyn_zoo, "penguin", 12, "howie")
+alexander = Animal.new(brooklyn_zoo, "zebra", 350, "lexie")
+jeffery = Animal.new(san_diego_zoo, "elephant", 2000, "jeff")
+jennifer = Animal.new(san_diego_zoo, "giraffe", 1500, "jenny")
+iris = Animal.new(chicago_zoo, "penguin", 7, "riri")
+zipporah = Animal.new(chicago_zoo, "penguin", 9, "zippy")
+elaine = Animal.new(chicago_zoo, "elephant", 2700, "ellie")
 
 
 binding.pry
 puts "done"
+
+## to test: ruby run.rb then run each method to make sure it works correctly :)
