e32f4969b3102a0e7408082554ae5d06c2afab15
noam sauer-utley <noamsauerutley@gmail.com>
2019-09-19 19:28:52 +0000

    Automatically backed up by Learn

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,5 +1,5 @@
 
-### Animal Class
+### Animal Class Deliverables:
 # - An `animal` should be instantiated with the species (e.g. "Cat", "Dog", "Rat"), a numerical weight and a nickname. Keep in mind that the animal's species and nickname should not be able to change, but its weight can.
 # - `Animal#nickname` should return the nickname of the animal.
 # - `Animal#weight` should return the weight of the animal.
